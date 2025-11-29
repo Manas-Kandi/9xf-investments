ff8edc802c13d72027dad810af1955885ef4f7d3
Manas Kandimalla <manaskandimalla2002@gmail.com>
2025-11-29 14:50:08 -0600

    Add tooling scripts and update documentation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
+import reactHooks from "eslint-plugin-react-hooks";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +15,31 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      import: importPlugin,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      "import/no-duplicates": "error",
+      "import/order": [
+        "error",
+        {
+          alphabetize: { order: "asc", caseInsensitive: true },
+          "newlines-between": "always",
+          pathGroupsExcludedImportTypes: ["builtin"],
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            ["parent", "sibling", "index"],
+            "type",
+          ],
+        },
+      ],
+      "react-hooks/exhaustive-deps": "error",
+      "react-hooks/rules-of-hooks": "error",
+    },
+  },
 ]);
 
 export default eslintConfig;
